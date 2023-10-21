62acea02048fec871677ffec14a840d15985ed91
Sean <seanwilliamogara@gmail.com>
2023-10-21 22:24:37 +0100

    added zoom in and out functionality

--- planet_simulator.py
+++ planet_simulator.py
@@ -14,7 +14,7 @@ SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 1200
 # Create the screen object
 screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
-
+zoom = 1
 # Setup the clock for a decent framerate
 clock = pygame.time.Clock()
 FPS = 60
@@ -29,8 +29,6 @@ PLANET_FONT = pygame.font.SysFont("cambriamath", 20)
 #https://ssd.jpl.nasa.gov/horizons/app.html#/
 #https://ssd-api.jpl.nasa.gov/doc/horizons.html
 
-#add zoom feature by multpling object plot coordianates by zoom factor (after adjusting for the non 0 origin in pygame)
-
 class Planet(object):
     def __init__(self, name, m, r, colour, pos, vel, accel = [0 , 0]):
         self.name = name
@@ -43,6 +41,9 @@ class Planet(object):
         self.y_vel = vel[1] 
         self.x_accel = accel[0] 
         self.y_accel = accel[1] 
+        self.x_scaled = self.x_pos
+        self.y_scaled = self.y_pos
+        self.radius_scaled = self.radius
         
     def get_planet_name(self):
         return self.name
@@ -75,35 +76,44 @@ class Planet(object):
         
     def calc_force(self, pos_other, mass_other):
         r_distance = self.calc_distance(pos_other)
-        unit_vector = [((pos_other[0] - self.x_pos) / r_distance), ((pos_other[1] - self.y_pos) / r_distance)]
-        
-        magnitude = G * mass_other * self.mass / (r_distance ** 2)# 1/r^2 for now
-        
+        unit_vector = [((pos_other[0] - self.x_pos) / r_distance), ((pos_other[1] - self.y_pos) / r_distance)]     
+        magnitude = G * mass_other * self.mass / (r_distance ** 2)
         x_force = unit_vector[0] * magnitude
         y_force = unit_vector[1] * magnitude
         force = [x_force, y_force]     
-        return force    
+        return force 
+
+    def set_scale_zoom_distances(self):
+        self.x_scaled = int((self.x_pos / DISTANCE_SCALE - SCREEN_WIDTH / 2) * zoom + SCREEN_WIDTH / 2)
+        self.y_scaled = int((self.y_pos / DISTANCE_SCALE - SCREEN_HEIGHT / 2) * zoom + SCREEN_WIDTH / 2)
+        self.radius_scaled = int(self.radius * zoom)
+
+    def get_scaled_pos(self):
+        return[self.x_scaled, self.y_scaled]
     
     def draw_planet(self): 
+        self.set_scale_zoom_distances()
         #draw path
         for i, point in enumerate(path_points[self]):
             #only draw point outside of planet radius
             planet_point_distance = self.calc_distance([point[0] * DISTANCE_SCALE, point[1] * DISTANCE_SCALE])
-            if planet_point_distance > (self.radius * DISTANCE_SCALE):
+            if planet_point_distance > (self.radius_scaled * DISTANCE_SCALE):
                 pygame.draw.circle(screen, self.colour, (point[0], point[1]), 1)
 
-        path_points[self].append([self.x_pos / DISTANCE_SCALE, self.y_pos / DISTANCE_SCALE])
-        #draw planet
-        pygame.draw.circle(screen, self.colour, (self.x_pos / DISTANCE_SCALE, self.y_pos / DISTANCE_SCALE), self.radius)
-        
+        path_points[self].append([self.x_scaled, self.y_scaled])
+        # Draw planet
+        pygame.draw.circle(screen, self.colour, (self.x_scaled, self.y_scaled), self.radius_scaled)
     def draw_planet_text(self):
+            scaled_x = int((self.x_pos / DISTANCE_SCALE - SCREEN_WIDTH / 2) * zoom + SCREEN_WIDTH / 2)
+            scaled_y = int((self.y_pos / DISTANCE_SCALE - SCREEN_HEIGHT / 2) * zoom + SCREEN_WIDTH / 2)
+            scaled_radius = int(self.radius * zoom)
             text = PLANET_FONT.render(self.get_planet_name(), False, RGB_COLOURS["WHITE"])
-            screen.blit(text, (self.x_pos / DISTANCE_SCALE - 18, self.y_pos / DISTANCE_SCALE - self.radius - 26))
+            screen.blit(text, (scaled_x - 18, scaled_y - scaled_radius - 26))
 
-#3-body system
-#Earth = Planet(6 * 10 ** 24, 10, RGB_COLOURS["BLUE"], [100 + SCREEN_WIDTH / 2, SCREEN_HEIGHT/ 2] , [-5 * 10 ** 3 , 2 * 10 ** 4])
-#Moon = Planet(7 * 10 ** 22, 6, RGB_COLOURS["WHITE"], [120 + SCREEN_WIDTH / 2, SCREEN_HEIGHT/ 2] , [-5.1 * 10 ** 3 , 2.1 * 10 ** 4])
-#Sun = Planet("Sun", 2 * 10 ** 30, 15, RGB_COLOURS["ORANGE"], [SCREEN_WIDTH / 2, SCREEN_HEIGHT/ 2] , [0, 0])
+def set_zoom(scroll_increment, previous_zoom):
+    zoom = previous_zoom + scroll_increment / 10
+    print(zoom)
+    return zoom
 
 #REALISTIC MASSESS/Orbital veloicties
 Mercury = Planet("Mecury", 3.3 * 10 ** 23, 4, RGB_COLOURS["GREY"], [38 + SCREEN_WIDTH / 2, SCREEN_HEIGHT/ 2] , [0, 4.7 * 10 ** 4])
@@ -127,7 +137,9 @@ while running:
     # Did the user click the window close button?
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False      
+            running = False   
+        elif event.type == pygame.MOUSEWHEEL:
+            zoom = set_zoom(scroll_increment=event.y, previous_zoom=zoom)    
     for Planet_i in planet_list:
         force = [0, 0]
         for Planet_j in planet_list:
@@ -140,9 +152,8 @@ while running:
 
         Planet_i.update_accel(accel)
         Planet_i.draw_planet()
-    
         Mouse_x, Mouse_y = pygame.mouse.get_pos()
-        if ((Planet_i.get_pos()[0] / DISTANCE_SCALE - 10 <= Mouse_x <= Planet_i.get_pos()[0] / DISTANCE_SCALE + 10) and (Planet_i.get_pos()[1] / DISTANCE_SCALE - 10  <= Mouse_y <= Planet_i.get_pos()[1] / DISTANCE_SCALE + 10)):
+        if ((Planet_i.get_scaled_pos()[0] - 10 <= Mouse_x <= Planet_i.get_scaled_pos()[0] + 10) and (Planet_i.get_scaled_pos()[1] - 10  <= Mouse_y <= Planet_i.get_scaled_pos()[1] + 10)):
             Planet_i.draw_planet_text()  
     pygame.display.update()
 pygame.quit()
